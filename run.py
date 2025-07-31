0bbfa5c22a09a9801ecaedb55bad78eec8062c96
Nikita Demir <nikitde1@gmail.com>
2025-07-31 10:47:24 -0400

    fixed

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ app = modal.App("nvs-leaderboard-downloader")
 
 @app.function(
     image=modal.Image.debian_slim().apt_install("wget", "unzip").add_local_dir(".", remote_path="/root/workspace"),
-    volumes={"/nvs-leaderboard": modal.Volume.from_name("nvs-leaderboard", create_if_missing=True)},
+    volumes={"/nvs-leaderboard-data": modal.Volume.from_name("nvs-leaderboard-data", create_if_missing=True),
+             "/nvs-leaderboard-output": modal.Volume.from_name("nvs-leaderboard-output", create_if_missing=True)
+    },
     timeout=3600, 
 )
 def download_dataset():
-    os.system("cd /nvs-leaderboard/data && bash /root/workspace/dataset_downloads/mipnerf360.sh")
+    os.system("cd /nvs-leaderboard-data && bash /root/workspace/dataset_downloads/mipnerf360.sh")
