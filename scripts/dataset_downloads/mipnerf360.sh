723365d8b188ae79853f29554fab57cad170f6f1
Nikita Demir <nikitde1@gmail.com>
2025-08-15 20:52:40 +0000

    small fix

--- scripts/dataset_downloads/mipnerf360.sh
+++ scripts/dataset_downloads/mipnerf360.sh
@@ -1,19 +1,23 @@
 #! /bin/bash
 
+cd /nvs-leaderboard-downloads/
+
 # Download dataset
-wget http://storage.googleapis.com/gresearch/refraw360/360_v2.zip
-wget https://storage.googleapis.com/gresearch/refraw360/360_extra_scenes.zip
+if [ ! -f 360_v2.zip ]; then
+    wget http://storage.googleapis.com/gresearch/refraw360/360_v2.zip
+fi
+if [ ! -f 360_extra_scenes.zip ]; then
+    wget https://storage.googleapis.com/gresearch/refraw360/360_extra_scenes.zip
+fi
 
-unzip 360_v2.zip -d mipnerf360
-unzip 360_extra_scenes.zip -d mipnerf360
-
-rm 360_v2.zip 360_extra_scenes.zip
+unzip 360_v2.zip -d /nvs-leaderboard-data/mipnerf360
+unzip 360_extra_scenes.zip -d /nvs-leaderboard-data/mipnerf360
 
 rm flowers.txt treehill.txt # These were disclaimers from the original dataset that the extra_scenes were not yet available
 
 # Remove the different resolution image folders and select the remaining images folder as:
 # outdoor scenes: images_4 -> images, indoor scenes: images_2 -> images
-cd mipnerf360
+cd /nvs-leaderboard-data/mipnerf360
 for scene in */; do
     cd "$scene"
     case "$(basename "$scene")" in
