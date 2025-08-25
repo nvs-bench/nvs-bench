0841fc3035676c3695c140722eebcfb0c4b24a4b
Nikita Demir <nikitde1@gmail.com>
2025-08-25 10:40:45 -0400

    Update link styling in NvsBenchTable component for consistency

--- website/components/nvs-bench-table.tsx
+++ website/components/nvs-bench-table.tsx
@@ -249,7 +249,7 @@ export function NvsBenchTable() {
                       href={methodMeta.method_url}
                       target="_blank"
                       rel="noreferrer"
-                      className="text-blue-600 hover:text-blue-800 underline underline-offset-2"
+                      className="text-foreground hover:text-foreground/80 underline underline-offset-2"
                     >
                       {methodMeta.method_display_name}
                     </a>
