86c4bfd7b794c23274214705699b97868e589a31
Nikita Demir <nikitde1@gmail.com>
2025-08-29 13:58:19 -0400

    moved boilerplate files around

--- scripts/dataset_downloads/deepblending_and_tanksandtemples.sh
+++
@@ -1,6 +0,0 @@
-wget https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/datasets/input/tandt_db.zip /nvs-bench-downloads/
-
-unzip tandt_db.zip
-
-mv db /nvs-bench-data/deepblending/
-mv tandt /nvs-bench-data/tanksandtemples/
