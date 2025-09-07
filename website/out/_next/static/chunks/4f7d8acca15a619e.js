273f892c42e6828197768861f3633321a8041478
Nikita Demir <nikitde1@gmail.com>
2025-09-07 07:50:45 -0400

    adding the website build files to see it work on pages

---
+++ website/out/_next/static/chunks/4f7d8acca15a619e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/744b907b86113ddd.js",
+  "static/chunks/d8fa5a60076cde8c.js",
+  "static/chunks/turbopack-c842db5041d8edfb.js"
+])
---
+++ website/out/_next/static/chunks/623986d3e0d201e9.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/50d3930286471d60.js",
+  "static/chunks/d8fa5a60076cde8c.js",
+  "static/chunks/turbopack-88641347c304a06e.js"
+])
