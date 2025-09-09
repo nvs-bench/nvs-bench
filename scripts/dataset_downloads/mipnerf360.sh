45ae6ea23ac307200ba9ba44494eacfe4e4f6c27
Nikita Demir <nikitde1@gmail.com>
2025-09-09 12:04:31 -0400

    unoptimized working

--- scripts/dataset_downloads/mipnerf360.sh
+++ scripts/dataset_downloads/mipnerf360.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-cd /nvs-bench-downloads/
+mkdir -p /nvs-bench/temp_downloads/mipnerf360
+cd /nvs-bench/temp_downloads/mipnerf360
 
 # Download dataset
 if [ ! -f 360_v2.zip ]; then
@@ -10,23 +11,34 @@ if [ ! -f 360_extra_scenes.zip ]; then
     wget https://storage.googleapis.com/gresearch/refraw360/360_extra_scenes.zip
 fi
 
-unzip -o 360_v2.zip -d /nvs-bench-data/mipnerf360
-unzip -o 360_extra_scenes.zip -d /nvs-bench-data/mipnerf360
+unzip -o 360_v2.zip -d /nvs-bench/temp_downloads/mipnerf360
+unzip -o 360_extra_scenes.zip -d /nvs-bench/temp_downloads/mipnerf360
 
-rm /nvs-bench-data/mipnerf360/flowers.txt /nvs-bench-data/mipnerf360/treehill.txt # These were disclaimers from the original dataset that the extra_scenes were not yet available
+rm /nvs-bench/temp_downloads/mipnerf360/flowers.txt /nvs-bench/temp_downloads/mipnerf360/treehill.txt # These were disclaimers from the original dataset that the extra_scenes were not yet available
 
 # Remove the different resolution image folders and select the remaining images folder as:
 # outdoor scenes: images_4 -> images, indoor scenes: images_2 -> images
-cd /nvs-bench-data/mipnerf360
-for scene in */; do
-    cd "$scene"
+for scene in bicycle flowers garden stump treehill room counter kitchen bonsai; do
+    cd /nvs-bench/temp_downloads/mipnerf360/$scene
     case "$(basename "$scene")" in
-        bicycle|flowers|garden|stump|treehill) rm -rf images && mv images_4 images ;;
-        room|counter|kitchen|bonsai) rm -rf images && mv images_2 images ;;
+        bicycle|flowers|garden|stump|treehill) echo "Processing $scene: selecting images_4" && rm -rf images && mv images_4 images ;;
+        room|counter|kitchen|bonsai) echo "Processing $scene: selecting images_2" && rm -rf images && mv images_2 images ;;
     esac
     rm -rf images_[0-9]*
-    cd ..
+    cd /nvs-bench/temp_downloads/mipnerf360
 done
 
 # Remove poses_bounds.npy files from all scene folders
 find . -name "poses_bounds.npy" -type f -delete
+
+rm -rf /nvs-bench/data/mipnerf360
+mkdir -p /nvs-bench/data/mipnerf360
+mv /nvs-bench/temp_downloads/mipnerf360/bicycle /nvs-bench/data/mipnerf360/bicycle
+mv /nvs-bench/temp_downloads/mipnerf360/flowers /nvs-bench/data/mipnerf360/flowers
+mv /nvs-bench/temp_downloads/mipnerf360/garden /nvs-bench/data/mipnerf360/garden
+mv /nvs-bench/temp_downloads/mipnerf360/stump /nvs-bench/data/mipnerf360/stump
+mv /nvs-bench/temp_downloads/mipnerf360/treehill /nvs-bench/data/mipnerf360/treehill
+mv /nvs-bench/temp_downloads/mipnerf360/room /nvs-bench/data/mipnerf360/room
+mv /nvs-bench/temp_downloads/mipnerf360/counter /nvs-bench/data/mipnerf360/counter
+mv /nvs-bench/temp_downloads/mipnerf360/kitchen /nvs-bench/data/mipnerf360/kitchen
+mv /nvs-bench/temp_downloads/mipnerf360/bonsai /nvs-bench/data/mipnerf360/bonsai
--- scripts/dataset_downloads/zipnerf.sh
+++ scripts/dataset_downloads/zipnerf.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-if [ ! -d /nvs-bench-downloads/zipnerf ]; then
-    mkdir -p /nvs-bench-downloads/zipnerf
-fi
-cd /nvs-bench-downloads/zipnerf
+mkdir -p /nvs-bench/temp_downloads/zipnerf
+cd /nvs-bench/temp_downloads/zipnerf
 
 if [ ! -f alameda.zip ]; then
     wget https://storage.googleapis.com/gresearch/refraw360/zipnerf-undistorted/alameda.zip
@@ -18,24 +16,24 @@ if [ ! -f nyc.zip ]; then
     wget https://storage.googleapis.com/gresearch/refraw360/zipnerf-undistorted/nyc.zip
 fi
 
-unzip -o alameda.zip -d /nvs-bench-downloads/zipnerf
-unzip -o berlin.zip -d /nvs-bench-downloads/zipnerf
-unzip -o london.zip -d /nvs-bench-downloads/zipnerf
-unzip -o nyc.zip -d /nvs-bench-downloads/zipnerf
+unzip -o alameda.zip -d /nvs-bench/temp_downloads/zipnerf
+unzip -o berlin.zip -d /nvs-bench/temp_downloads/zipnerf
+unzip -o london.zip -d /nvs-bench/temp_downloads/zipnerf
+unzip -o nyc.zip -d /nvs-bench/temp_downloads/zipnerf
 
 # Downsample factor 4 based on 3dgrut (these are large scenes after all)
 DATA_FACTOR=4
 
 for scene in alameda berlin london nyc; do
-    cd /nvs-bench-downloads/zipnerf/$scene
+    cd /nvs-bench/temp_downloads/zipnerf/$scene
     rm -rf images && mv images_$DATA_FACTOR images
     rm -rf images_[0-9]*
-    cd /nvs-bench-downloads/zipnerf
+    cd /nvs-bench/temp_downloads/zipnerf
 done
 
 rm -rf /nvs-bench/data/zipnerf
 mkdir -p /nvs-bench/data/zipnerf/
-mv /nvs-bench-downloads/zipnerf/alameda /nvs-bench/data/zipnerf/alameda
-mv /nvs-bench-downloads/zipnerf/berlin /nvs-bench/data/zipnerf/berlin
-mv /nvs-bench-downloads/zipnerf/london /nvs-bench/data/zipnerf/london
-mv /nvs-bench-downloads/zipnerf/nyc /nvs-bench/data/zipnerf/nyc
+mv /nvs-bench/temp_downloads/zipnerf/alameda /nvs-bench/data/zipnerf/alameda
+mv /nvs-bench/temp_downloads/zipnerf/berlin /nvs-bench/data/zipnerf/berlin
+mv /nvs-bench/temp_downloads/zipnerf/london /nvs-bench/data/zipnerf/london
+mv /nvs-bench/temp_downloads/zipnerf/nyc /nvs-bench/data/zipnerf/nyc
